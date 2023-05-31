db87577e2a76d48b8bd6a69e040154aebfb6f62a
Domingos Canhanga <domingos.canhanga@mamboo.co.ao>
2023-05-31 15:21:20 +0100

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/react',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh', 'prettier'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+    'prettier/prettier': ['error', { singleQuote: true, semi: true }],
+    '@typescript-eslint/no-empty-interface': 'off',
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <>
+      <div className="app bg-gray-20">App</div>
+    </>
+  );
+};
